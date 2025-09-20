1ced346b8b9c4e9120e441abcfa451b4ae82a71f
jailsondx <jailson_dx@hotmail.com>
2025-09-20 13:44:10 -0300

    Mais ajustes para o VERCEL

--- backend/server.js
+++ backend/server.js
@@ -20,3 +20,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+
+   export default app; // Exporta para Vercel Serverless
